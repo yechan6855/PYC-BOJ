9d82f05c18e4377ae836f55f5c844c6d1228de78
yechan6855 <136340169+yechan6855@users.noreply.github.com>
2023-12-28 21:35:37 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,i,a,b;
+    cin>>n;
+    for(i=1; i<=n; i++)
+    {
+    	cin>>a>>b;
+    	cout<<"Case #"<<i<<": "<<a+b<<endl;
+	}
+}
