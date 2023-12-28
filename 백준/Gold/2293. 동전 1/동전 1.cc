09a58c52830a0982502d13cb8857bcf9fa88d77e
yechan6855 <136340169+yechan6855@users.noreply.github.com>
2023-12-28 21:46:35 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> coin(n);
+    vector<int> dp(k + 1, 0);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> coin[i];
+    }
+
+    dp[0] = 1;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = coin[i]; j <= k; ++j) {
+            dp[j] += dp[j - coin[i]];
+        }
+    }
+
+    cout << dp[k] << endl;
+
+    return 0;
+}
