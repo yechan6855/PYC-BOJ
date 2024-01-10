362d50e03d3a14802116ce7af64b4cc4f0210b0c
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-01-11 00:42:05 +0900

    [Gold IV] Title: 최단경로, Time: 100 ms, Memory: 9192 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+typedef pair<int,int> edge;
+static int V,E,K;
+static vector<int> mdistance;
+static vector<bool> visited;
+static vector<vector<edge>> mlist;
+static priority_queue<edge,vector<edge>,greater<edge>> q;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin>>V>>E>>K;
+    mdistance.resize(V+1);
+    std::fill(mdistance.begin(),mdistance.end(),INT_MAX);
+    visited.reserve(V+1);
+    std::fill(visited.begin(),visited.end(),false);
+    mlist.resize(V+1);
+
+    for(int i=0; i<E; i++){
+        int u,v,w;
+        cin>>u>>v>>w;
+        mlist[u].push_back(make_pair(v,w));
+    }
+
+    q.push(make_pair(0,K));
+    mdistance[K]=0;
+
+    while(!q.empty()){
+        edge current = q.top();
+        q.pop();
+        int c_v = current.second;
+        if(visited[c_v]){
+            continue;
+        }
+        visited[c_v] = true;
+
+        for(int i=0; i<mlist[c_v].size(); i++){
+            edge tmp = mlist[c_v][i];
+            int next = tmp.first;
+            int value = tmp.second;
+
+            if(mdistance[next]>mdistance[c_v]+value){
+                mdistance[next]=value+mdistance[c_v];
+                q.push(make_pair(mdistance[next],next));
+            }
+        }
+    }
+    for(int i=1; i<=V; i++){
+        if(visited[i]){
+            cout<<mdistance[i]<<"\n";
+        }
+        else{
+            cout<<"INF"<<"\n";
+        }
+    }
+}
