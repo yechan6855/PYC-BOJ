c4d3d2fdfe88f62c70f8b26211d87315e605add8
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-01-10 17:52:11 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector<int> a[1001];
+bool check[1001];
+
+void dfs(int node) {
+    check[node] = true;
+    cout << node << " ";
+    for (int i=0; i<a[node].size(); i++) {
+        int next = a[node][i];
+        if (check[next] == false) {
+            dfs(next);
+        }
+    }
+}
+void bfs(int start) {
+    queue<int> q;
+    check[start] = true;
+    q.push(start);
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        cout << node << " ";
+        for (int i=0; i<a[node].size(); i++) {
+            int next = a[node][i];
+            if (check[next] == false) {
+                check[next] = true;
+                q.push(next);
+            }
+        }
+    }
+}
+int main() {
+    int n, m, start;
+    cin >> n >> m >> start;
+    for (int i=0; i<m; i++) {
+        int u,v;
+        cin >> u >> v;
+        a[u].push_back(v);
+        a[v].push_back(u);
+    }
+    for (int i=1; i<=n; i++) {
+        sort(a[i].begin(), a[i].end());
+    }
+    dfs(start);
+    cout << '\n';
+    memset(check,false,sizeof(check));
+    bfs(start);
+    return 0;
+}
