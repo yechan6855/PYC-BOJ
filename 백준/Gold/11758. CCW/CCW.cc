b339746458a1b95ab41166132e1d73dc2b6e681f
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-01-14 13:14:03 +0900

    [Gold V] Title: CCW, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x1,y1,x2,y2,x3,y3;
+    cin>>x1>>y1>>x2>>y2>>x3>>y3;
+    if((x1*y2-x2*y1+x2*y3-x3*y2+x3*y1-x1*y3)/2>0){
+        cout<<"1\n";
+    }
+    else if((x1*y2-x2*y1+x2*y3-x3*y2+x3*y1-x1*y3)/2<0){
+        cout<<"-1\n";
+    }
+    else{
+        cout<<"0\n";
+    }
+}
