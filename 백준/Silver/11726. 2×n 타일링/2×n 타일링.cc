4099628ba80067b0d7e85071572fdb0d66f94fe4
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-01-16 02:00:24 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+const int MOD = 10007;
+int dp[1001];
+
+int main() {
+    int n;
+    cin >> n;
+
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; ++i) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+    }
+    cout << dp[n] << endl;
+}
