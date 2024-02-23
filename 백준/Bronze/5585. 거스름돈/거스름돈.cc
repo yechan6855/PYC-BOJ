cb279eec2a176508eea6b3b530b8b7643d269023
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-02-23 21:21:06 +0900

    [Bronze II] Title: 거스름돈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    n = 1000 - n;
+    int cnt = 0;
+    cnt += n / 500;
+    n %= 500;
+    cnt += n / 100;
+    n %= 100;
+    cnt += n / 50;
+    n %= 50;
+    cnt += n / 10;
+    n %= 10;
+    cnt += n / 5;
+    n %= 5;
+    cnt += n;
+    cout << cnt;
+    return 0;
+}
