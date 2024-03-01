d67ba5224dc999d923ba1eab9f87a8aad1536a6a
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-03-01 17:17:53 +0900

    [Silver II] Title: 연속합, Time: 28 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    vector<int> dp(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    dp[0] = arr[0];
+    for (int i = 1; i < n; i++) {
+        dp[i] = max(dp[i - 1] + arr[i], arr[i]);
+    }
+    cout << *max_element(dp.begin(), dp.end());
+}
