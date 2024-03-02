8881c4b0e04714e611809fc272e2c7f8a28f077d
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-03-02 12:23:58 +0900

    [Bronze III] Title: A+B - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    int a, b;
+    char c;
+
+    while (t--) {
+        cin >> a >> c >> b;
+        cout << a + b <<endl;
+    }
+    return 0;
+}
