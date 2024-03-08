be5fa55fb996f669f734513a17f6f0223dd31319
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-03-08 22:12:55 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int hp) {
+    int answer = 0;
+    while(hp>0){
+        if(hp/5>=1){
+            hp-=5;
+            answer++;
+        }else if(hp/3>=1){
+            hp-=3;
+            answer++;
+        }else{
+            hp--;
+            answer++;
+        }
+    }
+    return answer;
+}
