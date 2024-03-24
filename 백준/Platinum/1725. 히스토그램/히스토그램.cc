49b2c45d2cba5aa48ec345b6288fb7745dd956e7
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-03-24 14:43:49 +0900

    [Platinum V] Title: 히스토그램, Time: 48 ms, Memory: 2812 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int largestRectangleArea(vector<int>& heights) {
+    stack<int> s;
+    int maxArea = 0;
+    int n = heights.size();
+    for (int i = 0; i <= n; i++) {
+        while (!s.empty() && (i == n || heights[s.top()] > heights[i])) {
+            int height = heights[s.top()];
+            s.pop();
+            int width = s.empty() ? i : i - s.top() - 1;
+            maxArea = max(maxArea, height * width);
+        }
+        s.push(i);
+    }
+    return maxArea;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> heights(n);
+    for (int i = 0; i < n; i++) {
+        cin >> heights[i];
+    }
+    cout << largestRectangleArea(heights) << endl;
+    return 0;
+}
