58b87a0f4db38a94aa97d66e6b76e50c380d69b0
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-03-28 13:58:22 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << ":fan::fan::fan:" << endl;
+    cout << ":fan::" << s << "::fan:" << endl;
+    cout << ":fan::fan::fan:" << endl;
+    return 0;
+}
