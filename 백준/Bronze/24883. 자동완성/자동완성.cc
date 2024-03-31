1fd6192cb8ebad81c4a72916093445eaa69b7144
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-03-31 12:32:50 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string a;
+    cin>>a;
+
+    if(a=="N"|| a=="n"){
+        cout<<"Naver D2"<<endl;
+    }
+    else{
+        cout<<"Naver Whale"<<endl;
+    }
+}
