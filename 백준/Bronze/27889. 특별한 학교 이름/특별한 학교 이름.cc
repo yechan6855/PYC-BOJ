cfe4e34340178cde17416daf4a2f710a7e14710d
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-04-01 00:25:39 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    if(s=="NLCS"){
+        cout<<"North London Collegiate School"<<endl;
+    }else if(s=="BHA"){
+        cout<<"Branksome Hall Asia"<<endl;
+    }else if(s=="KIS"){
+        cout<<"Korea International School"<<endl;
+    }else if(s=="SJA"){
+        cout<<"St. Johnsbury Academy"<<endl;
+    }
+}
