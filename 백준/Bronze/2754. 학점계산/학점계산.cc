bc64b03247673a55eb5d600e0506dadc72ee6dfe
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-04-17 21:02:47 +0900

    [Bronze III] Title: 학점계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin>>s;
+    if(s=="A+"){
+        cout<<"4.3";
+    }else if(s=="A0"){
+        cout<<"4.0";
+    }else if(s=="A-"){
+        cout<<"3.7";
+    }else if(s=="B+"){
+        cout<<"3.3";
+    }else if(s=="B0"){
+        cout<<"3.0";
+    }else if(s=="B-"){
+        cout<<"2.7";
+    }else if(s=="C+"){
+        cout<<"2.3";
+    }else if(s=="C0"){
+        cout<<"2.0";
+    }else if(s=="C-"){
+        cout<<"1.7";
+    }else if(s=="D+"){
+        cout<<"1.3";
+    }else if(s=="D0"){
+        cout<<"1.0";
+    }else if(s=="D-"){
+        cout<<"0.7";
+    }else if(s=="F"){
+        cout<<"0.0";
+    }
+
+}
