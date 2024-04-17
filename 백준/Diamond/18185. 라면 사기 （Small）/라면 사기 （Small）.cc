182f30253612a4b0ec8cb32f405719b6bec3ac9e
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-04-17 18:55:45 +0900

    [Diamond V] Title: 라면 사기 (Small), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Diamond/18185. 라면 사기 （Small）/라면 사기 （Small）.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    long long cost = 0;
+    for (int i = 0; i < N; i++) {
+        if (i+2 < N) {
+            int diff = A[i+1] - A[i+2];
+            if (diff > 0) {
+                int count = min(A[i], diff);
+                cost += count * 5;
+                A[i] -= count;
+                A[i+1] -= count;
+            }
+
+            int count = min(A[i], min(A[i+1], A[i+2]));
+            cost += count * 7;
+            A[i] -= count;
+            A[i+1] -= count;
+            A[i+2] -= count;
+        }
+        if (i+1 < N) {
+            int count = min(A[i], A[i+1]);
+            cost += count * 5;
+            A[i] -= count;
+            A[i+1] -= count;
+        }
+        cost += A[i] * 3;
+    }
+
+    cout << cost << endl;
+    return 0;
+}
