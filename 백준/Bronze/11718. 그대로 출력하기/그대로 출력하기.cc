47a76b8492ee70e627f7f48ca549760e8103be09
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-04-17 21:09:59 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -1,15 +1,14 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-    string str;
-    
-    while (true){
-        getline(cin, str);
-        if (str == ""){
+int main() {
+    string s;
+    while(1){
+        getline(cin,s);
+        if(s==""){
             break;
         }
-        cout << str << endl;
+        cout<<s<<"\n";
     }
 }
