6b8311c48a85ff623c8822ad381bf1216864fee7
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-06-10 00:37:34 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -1,27 +1,28 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-using namespace std;
-int main(){
-    int n;
-    cin >> n;
-    string word;
-    int ans = 0;
-    for (int i = 0; i < n; i++){
-        bool isgroup = true;
-        cin >> word;
-        word.erase(unique(word.begin(), word.end()), word.end());
-        sort(word.begin(), word.end());
-        for (int k = 0; k < word.length()-1;k++){
-            if(word[k] == word[k+1]){
-                isgroup = false;
+#include <bits/stdc++.h>
+
+int main(void) {
+    int N;
+    scanf("%d", &N);
+
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        char word[101];
+        scanf("%s", word);
+
+        int visited[26] = {0};
+
+        int len = strlen(word);
+        int j;
+        for (j = 0; j < len; j++) {
+            if (j > 0 && word[j] != word[j - 1] && visited[word[j] - 'a'] == 1) {
                 break;
             }
+            visited[word[j] - 'a'] = 1;
         }
-        if(isgroup){
-            ans++;
+        if (j == len) {
+            count++;
         }
     }
-    cout << ans;
+    printf("%d\n", count);
     return 0;
 }
