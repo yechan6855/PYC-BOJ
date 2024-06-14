e7b6cb34e3797ad792d92ad3f5828f6aa9e598b8
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-06-14 15:00:11 +0900

    [Platinum V] Title: 버블 소트, Time: 148 ms, Memory: 6056 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.cc
+++ 백준/Platinum/1517. 버블 소트/버블 소트.cc
@@ -1,48 +1,70 @@
-//boj 1517
-
 #include <bits/stdc++.h>
-
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define pob pop_back
+#define pi pair<int,int>
+#define mi map<int,int>
+#define qi queue<int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define tiii tuple<int,int,int>
+#define endl "\n"
+#define io ios_base::sync_with_stdio(false); cin.tie(nullptr);
 using namespace std;
 
-int n;
-int arr[500001];
-int temp[500001];
-long long ans = 0;
+ll mrg_cnt(vi& a, int l, int m, int r) {
+    vi L(a.begin() + l, a.begin() + m + 1);
+    vi R(a.begin() + m + 1, a.begin() + r + 1);
 
-void merge(int start, int end) {
-    if (start == end) return;
-    int mid = (start + end) / 2;
-    merge(start, mid);
-    merge(mid + 1, end);
-    int i = start;
-    int j = mid + 1;
-    int k = 0;
-    while (i <= mid && j <= end) {
-        if (arr[i] <= arr[j]) {
-            temp[k++] = arr[i++];
-        }
-        else {
-            temp[k++] = arr[j++];
-            ans += (mid - i + 1);
+    int i = 0, j = 0, k = l;
+    ll cnt = 0;
+
+    while (i < L.size() && j < R.size()) {
+        if (L[i] <= R[j]) {
+            a[k++] = L[i++];
+        } else {
+            a[k++] = R[j++];
+            cnt += L.size() - i;
         }
     }
-    while (i <= mid) {
-        temp[k++] = arr[i++];
+
+    while (i < L.size()) {
+        a[k++] = L[i++];
     }
-    while (j <= end) {
-        temp[k++] = arr[j++];
+
+    while (j < R.size()) {
+        a[k++] = R[j++];
     }
-    for (int i = start; i <= end; i++) {
-        arr[i] = temp[i - start];
+
+    return cnt;
+}
+
+ll mrg_srt_cnt(vi& a, int l, int r) {
+    ll cnt = 0;
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        cnt += mrg_srt_cnt(a, l, m);
+        cnt += mrg_srt_cnt(a, m + 1, r);
+
+        cnt += mrg_cnt(a, l, m, r);
     }
+    return cnt;
 }
 
 int main() {
+    io;
+    int n;
     cin >> n;
+    vi a(n);
+
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        cin >> a[i];
     }
-    merge(0, n - 1);
-    cout << ans;
+
+    cout << mrg_srt_cnt(a, 0, n - 1) << endl;
+
     return 0;
 }
