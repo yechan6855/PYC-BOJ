e22daa4b4caea8b352d634cc6c6ebc7615217c89
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-07-04 10:34:55 +0900

    [Bronze V] Title: ゾロ目 (Same Numbers), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27324. ゾロ目 （Same Numbers）/ゾロ目 （Same Numbers）.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pb push_back
+#define fi first
+#define se second
+#define endl "\n"
+#define pii pair<int, int>
+#define vi vector<int>
+#define qi queue<int>
+#define vvi vector<vector<int>>
+#define vvvi vector<vector<vector<int>>>
+#define io ios_base::sync_with_stdio(false); cin.tie(nullptr)
+using namespace std;
+
+int32_t main()
+{
+    int n;
+    cin>>n;
+    cout<<(n/10==n%10);
+}
