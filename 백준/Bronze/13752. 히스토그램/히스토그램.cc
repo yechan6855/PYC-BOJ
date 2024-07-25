f58fdc97500d60c9f16f77af6ee0ecde65163f33
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-07-25 13:17:31 +0900

    [Bronze IV] Title: 히스토그램, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define io ios_base::sync_with_stdio(false);cin.tie(nullptr);
+using namespace std;
+
+int main(){
+    int t;
+    int n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        for(int i=0; i<n; i++){
+            cout<<"=";
+        }
+        cout<<endl;
+    }
+}
