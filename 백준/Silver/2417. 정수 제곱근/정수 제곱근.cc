8369a7f44c3e327fb9076ee57b65a8e2580aad36
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-07-26 15:16:14 +0900

    [Silver IV] Title: 정수 제곱근, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
@@ -1,34 +1,20 @@
 #include <bits/stdc++.h>
-#define ull unsigned long long
+#define ll long long
 #define endl "\n"
 #define io ios_base::sync_with_stdio(false);cin.tie(nullptr);
 using namespace std;
 
 int main() {
     io;
-    ull n;
+    ll n;
     cin >> n;
-    if(n==0){
-        cout<<0;
-        return 0;
+    
+    ll k = static_cast<ll>(sqrt(n));
+    
+    if (k * k < n) {
+        k++;
     }
-    ull l = 0;
-    ull r = sqrt(n) + 1;
-    ull ans = r;
-
-    while (l <= r) {
-        ull mid = (l + r) / 2;
-        ull square = mid * mid;
-
-        if (square >= n) {
-            ans = mid;
-            r = mid - 1;
-        } else {
-            l = mid + 1;
-        }
-    }
-
-    cout << ans << endl;
-
+    
+    cout << k << endl;
     return 0;
 }
