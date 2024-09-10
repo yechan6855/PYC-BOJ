c07c9c13bd34be012aa26af0a72d75643d523c66
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-09-10 18:58:20 +0900

    [Gold IV] Title: 음식 평론가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1188. 음식 평론가/음식 평론가.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N, M;
+    cin >> N >> M;
+
+    cout << M - gcd(N, M) << "\n";
+
+    return 0;
+}
