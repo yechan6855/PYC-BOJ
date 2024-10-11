12b524e4f909b7b44c1e8471cd350a6c42a6153a
박예찬 <136340169+yechan6855@users.noreply.github.com>
2024-10-11 09:43:48 +0900

    [Bronze V] Title: Just Round Down, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    double n;
+    cin>>n;
+    cout<<(int)n;
+
+    return 0;
+}
