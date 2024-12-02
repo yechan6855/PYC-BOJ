55e09ad66f75b76dd29c4b6a2e590ca16e434804
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2024-12-02 17:41:38 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int s, f;
+    cin >> s >> f;
+
+    if (s > f) {
+        cout << "flight";
+    } else {
+        cout << "high speed rail";
+    }
+
+    return 0;
+}
