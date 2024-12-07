c0bf5671369f0a09afd8470600d0ea9e73ac4824
Yechan Park <136340169+yechan6855@users.noreply.github.com>
2024-12-07 11:13:52 +0900

    [Silver IV] Title: 카드, Time: 132 ms, Memory: 8228 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.cc
@@ -0,0 +1,2 @@
+#include<bits/stdc++.h>
+using namespace std;int main(){int n;cin>>n;map<long long,int>m;while(n--){long long x;cin>>x;m[x]++;}int mx=0;long long ans;for(auto&i:m)if(i.second>mx)mx=i.second,ans=i.first;cout<<ans;}
